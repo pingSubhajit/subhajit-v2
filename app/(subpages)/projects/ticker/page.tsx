86a48430f3b2308c86c2e8065111abebc63cdee9
Subhajit Kundu <subha60kundu@gmail.com>
2024-06-11 08:04:03 +0530

    style: Adding responsiveness and hero parallax for Ticker project page

--- app/(subpages)/projects/ticker/page.tsx
+++ app/(subpages)/projects/ticker/page.tsx
@@ -29,42 +29,46 @@ const TickerProjectPage = () => {
 
 			<div className="grid grid-cols-12 gap-5 mt-40">
 				<h1 className="text-black font-medium text-8xl mt-16 col-start-3 col-span-8 -translate-x-2">Ticker</h1>
-				<h2 className="text-black text-3xl col-start-3 col-span-8 mb-32">Realtime long duration stopwatch</h2>
+				<h2 className="text-black text-3xl col-start-3 col-span-8 mb-24 lg:mb-32">Realtime long duration stopwatch</h2>
 
-				<div className="col-span-2 col-start-3 mb-32">
+				<div className="col-start-3 col-span-8 lg:col-span-2 lg:col-start-3 mb-8 lg:mb-32">
 					<h4 className="text-ghidorah text-xs font-medium uppercase">ROLE / SERVICES</h4>
 					<div className="w-full h-[1px] bg-beige my-4"/>
 					<p className="line-clamp-1 text-petroleum text-lg">Design, Development & Deploy</p>
 				</div>
 
-				<div className="col-span-2 col-start-6 mb-32">
+				<div className="col-start-3 col-span-8 lg:col-span-2 lg:col-start-6 mb-8 lg:mb-32">
 					<h4 className="text-ghidorah text-xs font-medium uppercase">CREDITS</h4>
 					<div className="w-full h-[1px] bg-beige my-4"/>
 					<p className="line-clamp-1 text-petroleum text-lg">Design inspiration: Matt Wojtaś</p>
 				</div>
 
-				<div className="col-span-2 col-start-9 mb-32">
+				<div className="col-start-3 col-span-8 lg:col-span-2 lg:col-start-9 mb-24 lg:mb-32">
 					<h4 className="text-ghidorah text-xs font-medium uppercase">PURPOSE</h4>
 					<div className="w-full h-[1px] bg-beige my-4"/>
 					<p className="line-clamp-1 text-petroleum text-lg">Personal requirement</p>
 				</div>
 
-				<Parallax speed={0.5}>
+				<Parallax speed={0.5} className="z-10">
 					<a href="https://tickernow.xyz" target="_blank">
-						<Button className="w-44 aspect-square bg-black text-beige rounded-full flex items-center
+						<Button className="w-28 lg:w-44 aspect-square bg-black text-beige rounded-full flex items-center
 						justify-center cursor-pointer whitespace-nowrap absolute right-36 top-28 [&>p]:flex
-						[&>p]:justify-center [&>p]:items-center [&>p]:gap-1">
+						[&>p]:justify-center [&>p]:items-center [&>p]:gap-1 text-sm lg:text-base">
 							<p>Live app</p>
 							<MoveUpRight className="w-4 h-4" />
 						</Button>
 					</a>
 				</Parallax>
 
-				<Image
-					src={tickerHero}
-					alt="Pretty looking mockup of the Ticker app presented in an iPhone and in an iPad"
-					className="col-span-10 col-start-2"
-				/>
+				<div className="aspect-[1.95] col-span-10 col-start-2 w-full bg-basil overflow-y-hidden">
+					<Parallax speed={-1.05}>
+						<Image
+							src={tickerHero}
+							alt="Pretty looking mockup of the Ticker app presented in an iPhone and in an iPad"
+							className="col-span-12 col-start-1"
+						/>
+					</Parallax>
+				</div>
 
 				<video width="2160" height="1238"
 					controls={false} preload="none"
@@ -75,7 +79,7 @@ const TickerProjectPage = () => {
 					Your browser does not support the video tag.
 				</video>
 
-				<div className="col-span-full bg-petroleum py-52 text-beige grid grid-cols-12 gap-5" >
+				<div className="col-span-full bg-petroleum py-24 lg:py-52 text-beige grid grid-cols-12 gap-5" >
 					<ProjectDescription
 						description={'A small tool for creating stopwatch timers that can run for days, ' +
 						'months, or even years. All synced in real-time across multiple devices. Loads fast and ' +
@@ -83,25 +87,25 @@ const TickerProjectPage = () => {
 						className="text-center col-start-3 col-span-8"
 					/>
 
-					<div className="mt-36 col-start-3 col-span-8 flex justify-between items-center">
-						<div className="h-44 w-44 bg-basil rounded-full flex items-center justify-center p-8">
-							<p className="text-white font-medium text-center text-balance">NextJS 15</p>
+					<div className="mt-24 lg:mt-36 col-start-2 col-span-10 lg:col-start-3 lg:col-span-8 flex flex-wrap justify-between items-center gap-4">
+						<div className="h-32 w-32 lg:h-44 lg:w-44 bg-basil rounded-full flex items-center justify-center p-8">
+							<p className="text-xs lg:text-base text-white font-medium text-center text-balance">NextJS 15</p>
 						</div>
-						<div className="h-44 w-44 bg-basil rounded-full flex items-center justify-center p-8">
-							<p className="text-white font-medium text-center text-balance">React Compiler</p>
+						<div className="h-32 w-32 lg:h-44 lg:w-44 bg-basil rounded-full flex items-center justify-center p-8">
+							<p className="text-xs lg:text-base text-white font-medium text-center text-balance">React Compiler</p>
 						</div>
 						<Link href="/writings/partial-prerendering">
 							<Magnetic>
-								<div className="h-44 w-44 bg-basil rounded-full flex items-center justify-center p-8">
-									<p className="text-white font-medium text-center text-balance">Partial Pre-rendering</p>
+								<div className="h-32 w-32 lg:h-44 lg:w-44 bg-basil rounded-full flex items-center justify-center p-8">
+									<p className="text-xs lg:text-base text-white font-medium text-center text-balance">Partial Pre-rendering</p>
 								</div>
 							</Magnetic>
 						</Link>
-						<div className="h-44 w-44 bg-basil rounded-full flex items-center justify-center p-8">
-							<p className="text-white font-medium text-center text-balance">Real-time changes</p>
+						<div className="h-32 w-32 lg:h-44 lg:w-44 bg-basil rounded-full flex items-center justify-center p-8">
+							<p className="text-xs lg:text-base text-white font-medium text-center text-balance">Real-time changes</p>
 						</div>
-						<div className="h-44 w-44 bg-basil rounded-full flex items-center justify-center p-8">
-							<p className="text-white font-medium text-center text-balance">Blazingly fast</p>
+						<div className="h-32 w-32 lg:h-44 lg:w-44 bg-basil rounded-full flex items-center justify-center p-8">
+							<p className="text-xs lg:text-base text-white font-medium text-center text-balance">Blazingly fast</p>
 						</div>
 					</div>
 				</div>
