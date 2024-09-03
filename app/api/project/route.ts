d5b92f9c5b088375ab5a9fc3b3fdc8c6ed9136e7
Subhajit Kundu <subha60kundu@gmail.com>
2024-09-03 13:17:52 +0530

    fix: Fixing CORS error for api route

--- app/api/project/route.ts
+++ app/api/project/route.ts
@@ -3,24 +3,11 @@ import {NextRequest, NextResponse} from 'next/server'
 export async function POST(req: NextRequest, res: NextResponse) {
 	try {
 		const body = await req.json()
-		if (!body.email) {
-			return NextResponse.json({
-				status: 'error',
-				message: 'Email is required'
-			})
-		}
 
-		return new Response(JSON.stringify({
+		process.stdout.write(body)
+		return NextResponse.json({
 			status: 'success',
-			email: body.email,
-			message: `${body.email} has been added to the project`
-		}), {
-			status: 200,
-			headers: {
-				'Access-Control-Allow-Origin': '*',
-				'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-				'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-			}
+			message: 'Data received'
 		})
 	} catch (error: any) {
 		return NextResponse.json({
